d73b7dfda9d156ffa39821b88a0ab47d469950e4
Dani <danimg_96@hotmail.com>
2020-11-26 14:30:29 +0100

    afegida funcionalitat de fleches al master

--- Robot/MainWindow.xaml.cs
+++ Robot/MainWindow.xaml.cs
@@ -24,7 +24,9 @@ namespace Robot
         Point tresor;
         ImageBrush imatgeRobot = new ImageBrush();
         ImageBrush imatgeTresor = new ImageBrush();
-        Brush snakeColor = Brushes.Green;
+        ImageBrush imgFlecha = new ImageBrush();
+        string flechaDireccio = "flechaDreta.png";
+
         enum DIRECCIO
         {
             NORD = 8,
@@ -32,26 +34,30 @@ namespace Robot
             OEST = 4,
             EST = 6
         };
-        Point puntInici = new Point(200, 200);
-        Point posicioActual = new Point();
+        Point puntIniciRobot = new Point(250, 250);
+        Point posicioActualRobot = new Point();
+        Point puntIniciFlecha = new Point(290, 290);
+        Point posicioActualFlecha = new Point();
         int direccio = 2;
         int direccioPrevia = 2;
         int tamanyRobot = 100;
         int llargada = 1;
-        int punts = 0;
         Random rnd = new Random();
         public MainWindow()
         {
             InitializeComponent();
             imatgeRobot.ImageSource = new BitmapImage(new Uri("robot.png", UriKind.Relative));
             imatgeTresor.ImageSource = new BitmapImage(new Uri("tresor.png", UriKind.Relative));
+            imgFlecha.ImageSource = new BitmapImage(new Uri(flechaDireccio, UriKind.Relative));
             DispatcherTimer timer = new DispatcherTimer();
             timer.Tick += new EventHandler(timer_Tick);
             TimeSpan velocitat = new TimeSpan(800000);
             timer.Interval = velocitat;
             timer.Start();
-            pintaRobot(puntInici);
-            posicioActual = puntInici;
+            pintaRobot(puntIniciRobot);
+            pintaFlecha(puntIniciFlecha);
+            posicioActualRobot = puntIniciRobot;
+            posicioActualFlecha = puntIniciFlecha;
             pintaTresor(0);
         }
         private void pintaRobot(Point posicio)
@@ -67,9 +73,27 @@ namespace Robot
 
             paintCanvas.Children.Add(robot);
 
-            if (paintCanvas.Children.Count - 1 > llargada)
+            if (paintCanvas.Children.Count - 2 > llargada)
             {
-                paintCanvas.Children.RemoveAt(paintCanvas.Children.Count - (llargada + 1));
+                paintCanvas.Children.RemoveAt(paintCanvas.Children.Count - (llargada + 2));
+            }
+        }
+        private void pintaFlecha(Point posicio)
+        {
+
+            Ellipse flecha = new Ellipse();
+            flecha.Fill = imgFlecha;
+            flecha.Width = 20;
+            flecha.Height = 20;
+
+            Canvas.SetTop(flecha, posicio.Y);
+            Canvas.SetLeft(flecha, posicio.X);
+
+            paintCanvas.Children.Add(flecha);
+
+            if (paintCanvas.Children.Count - 2 > llargada)
+            {
+                paintCanvas.Children.RemoveAt(paintCanvas.Children.Count - (llargada + 2));
             }
         }
         private void pintaTresor(int index)
@@ -89,16 +113,28 @@ namespace Robot
         private void timer_Tick(object sender, EventArgs e)
         {
             // Determina que el robot torni pel costat contrari al que ha sortit.
-            if (posicioActual.X < 10 && direccio == (int)DIRECCIO.OEST)
-                posicioActual.X = 500;
-            else if (posicioActual.X > 480 && direccio == (int)DIRECCIO.EST)
-                posicioActual.X = -10;
-            else if (posicioActual.Y < 10 && direccio == (int)DIRECCIO.NORD)
-                posicioActual.Y = 500;
-            else if (posicioActual.Y > 480 && direccio == (int)DIRECCIO.SUD)
-                posicioActual.Y = -10;
+            if (posicioActualRobot.X < 10 && direccio == (int)DIRECCIO.OEST)
+            {
+                posicioActualRobot.X = 500;
+                posicioActualFlecha.X = 540;
+            }
+            else if (posicioActualRobot.X > 480 && direccio == (int)DIRECCIO.EST)
+            {
+                posicioActualRobot.X = -10;
+                posicioActualFlecha.X = 30;
+            }
+            else if (posicioActualRobot.Y < 10 && direccio == (int)DIRECCIO.NORD)
+            {
+                posicioActualRobot.Y = 500;
+                posicioActualFlecha.Y = 540;
+            }
+            else if (posicioActualRobot.Y > 480 && direccio == (int)DIRECCIO.SUD)
+            {
+                posicioActualRobot.Y = -10;
+                posicioActualFlecha.Y = 30;
+            }
 
-            Moviment();
+            Direccio();
             // Mou el robot en la direccio del moviment.
             Random r = new Random();
             if (r.Next(0, 2) == 1)
@@ -106,61 +142,71 @@ namespace Robot
                 switch (direccio)
                 {
                     case (int)DIRECCIO.SUD:
-                        posicioActual.Y += 50;
-                        pintaRobot(posicioActual);
+                        posicioActualRobot.Y += 50;
+                        posicioActualFlecha.Y += 50;
+                        pintaRobot(posicioActualRobot);
+                        pintaFlecha(posicioActualFlecha);
                         break;
                     case (int)DIRECCIO.NORD:
-                        posicioActual.Y -= 50;
-                        pintaRobot(posicioActual);
+                        posicioActualRobot.Y -= 50;
+                        posicioActualFlecha.Y -= 50;
+                        pintaRobot(posicioActualRobot);
+                        pintaFlecha(posicioActualFlecha);
                         break;
                     case (int)DIRECCIO.OEST:
-                        posicioActual.X -= 50;
-                        pintaRobot(posicioActual);
+                        posicioActualRobot.X -= 50;
+                        posicioActualFlecha.X -= 50;
+                        pintaRobot(posicioActualRobot);
+                        pintaFlecha(posicioActualFlecha);
                         break;
                     case (int)DIRECCIO.EST:
-                        posicioActual.X += 50;
-                        pintaRobot(posicioActual);
+                        posicioActualRobot.X += 50;
+                        posicioActualFlecha.X += 50;
+                        pintaRobot(posicioActualRobot);
+                        pintaFlecha(posicioActualFlecha);
                         break;
                 }
             }
-            if ((Math.Abs(tresor.X - posicioActual.X) < tamanyRobot) &&
-                (Math.Abs(tresor.Y - posicioActual.Y) < tamanyRobot))
+            if ((Math.Abs(tresor.X - posicioActualRobot.X) < tamanyRobot) &&
+                (Math.Abs(tresor.Y - posicioActualRobot.Y) < tamanyRobot))
             {
                 GameOver();
             }
         }
-        private void Moviment() //Determina de manera random quan el robot es mou.
+        private void Direccio() //Determina de manera random la direccio.
         {
             int moneda;
             Random rnd = new Random();
             moneda = rnd.Next(0, 2);
-            if(moneda == 1)
+            while(moneda==0)
             {
                 if (direccioPrevia == (int)DIRECCIO.NORD)
                 {
-                    moneda = rnd.Next(0, 2);
-                    if (moneda == 1)
-                        direccio = (int)DIRECCIO.EST;
+                    direccio = (int)DIRECCIO.EST;
+                    flechaDireccio = "flechaDreta.png";
+                    imgFlecha.ImageSource = new BitmapImage(new Uri(flechaDireccio, UriKind.Relative));
+
                 }
 
                 else if (direccioPrevia == (int)DIRECCIO.EST)
                 {
-                    moneda = rnd.Next(0, 2);
-                    if (moneda == 1)
-                        direccio = (int)DIRECCIO.SUD;
+                    direccio = (int)DIRECCIO.SUD;
+                    flechaDireccio = "flechaAbaix.png";
+                    imgFlecha.ImageSource = new BitmapImage(new Uri(flechaDireccio, UriKind.Relative));
                 }
                 else if (direccioPrevia == (int)DIRECCIO.SUD)
                 {
-                    moneda = rnd.Next(0, 2);
-                    if (moneda == 1)
-                        direccio = (int)DIRECCIO.OEST;
+                    direccio = (int)DIRECCIO.OEST;
+                    flechaDireccio = "flechaEsquerra.png";
+                    imgFlecha.ImageSource = new BitmapImage(new Uri(flechaDireccio, UriKind.Relative));
                 }
                 else if (direccioPrevia == (int)DIRECCIO.OEST)
                 {
-                    moneda = rnd.Next(0, 2);
-                    if (moneda == 1)
-                        direccio = (int)DIRECCIO.NORD;
+                    direccio = (int)DIRECCIO.NORD;
+                    flechaDireccio = "flechaAdalt.png";
+                    imgFlecha.ImageSource = new BitmapImage(new Uri(flechaDireccio, UriKind.Relative));
                 }
+                moneda = rnd.Next(0, 2);
             }
             direccioPrevia = direccio;
         }
